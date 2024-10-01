a5e0c3cfe3812df0ea4f6a6ff02743d2e853b572
xmrsv <88881293+xmrsv@users.noreply.github.com>
2024-09-30 20:56:20 -0500

    Create basic config files

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+
+export default [
+	js.configs.recommended,
+
+	{
+		rules: {
+			"no-unused-vars": "off",
+			"no-undef": "off",
+            "yoda": "error",
+            "no-var": "error"
+		},
+	},
+];
